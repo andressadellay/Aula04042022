5e85458da39c0e66561b30eb2a20ea2c02eba425
[andressadellayagra] <[andressadellayagra@gmail.com]>
2022-04-04 19:31:50 -0300

    Enviando dois projetos de uma vez

---
+++ LimitadorDeCaractere2403/src/model/LimitadorNumeros.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+/**
+ *
+ * @author andre
+ */
+public class LimitadorNumeros extends PlainDocument {
+       private int quantidadeMax;
+    //construtor da classe - serve para inicializar variáveis da classe
+    public LimitadorNumeros(int max){
+        /*verifica se o que o programador definiu no GUIPessoa para quantidade
+        de caracteres é negativo ou nulo
+        */
+        if(max<=0){
+            throw new IllegalArgumentException("Erro ao definir quantidade de caracteres máximos");
+        /*caso o programador tenha definifo uma quantidade máxima de caracteres
+            que NÃO seja negativa ou nula, então, a variável global receberá o
+            parâmetro definido pelo programador.
+        */    
+        }else{
+            this.quantidadeMax = max;
+        }//fecha else
+    }//fecha método construtor
+
+    /*cria método inserString para verificar a quantidade de caracteres
+    que foi digitada dentro do campo JTextField.
+    */
+    @Override
+    public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+        /*
+        offs - posição do texto
+        str - texto que o usuário digita
+        a - atributo da fonte, ou seja, se aceita letras, números, caracteres especiais
+        */
+        /*
+        se o usuário não escreveu nada, então o usuário poderá prosseguir com seus dígitos
+        */
+        if(str == null){
+            return;
+        }
+        //soma o tamanho total do JTextField com o tamanho total de caracteres digitados pelo usuário
+        int totalQuantidade = (getLength()+str.length());
+        //se a soma da quantidade for menor ou então igual à quantidade máxia definida
+        if(totalQuantidade<=quantidadeMax){
+             super.insertString(offs, str.replaceAll("[^0-9]",""), a); //To change body of generated methods, choose Tools | Templates.
+             return;
+        }//fecha if
+        /*
+        cria uma variável String que receba os dados que o usuário estão digitando.
+        Continua mostrando(deixando visível) os caracteres que são permitidos. Caso usuário
+        digite carcteres não permitidos, não serão visíveis no JTextField
+        */
+        String nova = str.substring(0,getLength() - quantidadeMax );
+        super.insertString(offs, nova, a);
+    }//fecha método
+
+}
